8b80ad5a93403e01580274de6c6baf9ec6f2b46f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-02-07 19:28:55 -0500

    Se cambió el puerto db y se comentó la contraseña

--- models/DataBase.php
+++ models/DataBase.php
@@ -7,7 +7,7 @@
             $database = "db_tps_nc_iv_2771440";
             $username = "root";
             $password = "";
-            // $password
+            // $password = "blabla"
 			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
